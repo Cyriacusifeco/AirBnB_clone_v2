8fe3f3c67001f0b4ddc3e3c66a48eb3a33b7b439
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-04-20 22:09:07 +0000

    Add more routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,5 +15,19 @@ def index():
     return('Hello HBNB!')
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    view /bbnb
+    """
+    return('HBNB')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Display 'C <text>'"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
